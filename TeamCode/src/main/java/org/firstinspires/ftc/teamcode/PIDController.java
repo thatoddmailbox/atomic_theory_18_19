f8b38d30ff25395eea44ee54c618d2774fac074a
Alex Studer <astuder@notetoscreen.com>
2018-10-09 18:31:40 -0400

    add potentiometer code for elbow, with a PID controller that badly needs tuning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class PIDController {
+    public PIDCoefficients coefficients;
+
+    public double errorSum;
+
+    public double lastDError;
+
+    private ElapsedTime _runtime;
+    private double _lastError;
+    private double _lastTime;
+
+    public PIDController(PIDCoefficients _coefficients) {
+        coefficients = _coefficients;
+
+        _runtime = new ElapsedTime();
+
+        reset();
+    }
+
+    public void reset() {
+        errorSum = 0;
+        _lastError = 0;
+        _lastTime = 0;
+        _runtime.reset();
+    }
+
+    public double step(double current, double target) {
+        double deltaTime = (_runtime.milliseconds() - _lastTime);
+
+        double error = target - current;
+        errorSum += (error * deltaTime);
+        double dError = (error - _lastError) / deltaTime;
+
+        double output = coefficients.p * error + coefficients.i * errorSum + coefficients.d * dError;
+
+        lastDError = dError;
+        _lastError = error;
+        _lastTime = _runtime.milliseconds();
+
+        return output;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PotentiometerTest.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+
+@Autonomous(name="Potentiometer test")
+public class PotentiometerTest extends LinearOpMode {
+
+    AnalogInput elbowPotentiometer;
+    DcMotor elbowMotor;
+    PIDController elbowPID;
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+
+        elbowPotentiometer = hardwareMap.analogInput.get("elbow_pot");
+        elbowMotor = hardwareMap.dcMotor.get("elbow");
+
+        elbowMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        elbowPID = new PIDController(new PIDCoefficients(-1, -0.00005, 0));
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                moveToPosition_lessCrappy(0.5);
+            } else if (gamepad1.y) {
+                moveToPosition_crappy(0.5);
+            }
+
+            if (gamepad1.left_bumper) {
+                elbowMotor.setPower(.3);
+            } else if (gamepad1.right_bumper) {
+                elbowMotor.setPower(-.3);
+            } else {
+                elbowMotor.setPower(0);
+            }
+
+            telemetry.addData("reading", elbowPotentiometer.getVoltage());
+            telemetry.addData("position", getPosition());
+            telemetry.update();
+        }
+    }
+
+    public double getPosition() {
+        // 0 is closed, 1 is open
+        return ((elbowPotentiometer.getVoltage() - Consts.ELBOW_POSITION_CLOSED) / Consts.ELBOW_POSITION_OPEN);
+    }
+
+    public void moveToPosition_crappy(double target) {
+        while (opModeIsActive() && Math.abs(target - getPosition()) > 0.05) {
+            double current = getPosition();
+            int direction;
+            if (target > current) {
+                // up
+                direction = -1;
+            } else {
+                // down
+                direction = 1;
+            }
+            elbowMotor.setPower(direction * 0.3);
+
+            telemetry.addData("current", current);
+            telemetry.addData("target", target);
+            telemetry.update();
+        }
+    }
+
+    public void moveToPosition_lessCrappy(double target) {
+        elbowPID.reset();
+
+        while (opModeIsActive() && !gamepad1.b) {
+            double current = getPosition();
+            elbowMotor.setPower(elbowPID.step(current, target));
+
+            telemetry.addData("current", current);
+            telemetry.addData("target", target);
+            telemetry.addData("errorSum", elbowPID.errorSum);
+            telemetry.addData("lastDError", elbowPID.lastDError);
+            telemetry.update();
+        }
+    }
+}
