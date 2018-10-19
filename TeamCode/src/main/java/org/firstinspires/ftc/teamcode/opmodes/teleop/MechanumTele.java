52a4b92e0b097aafc1544aa352aa2a326c9c070d
Alex Studer <astuder@notetoscreen.com>
2018-10-19 17:37:11 -0400

    fix mechanum motor directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MechanumTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MechanumTele.java
@@ -19,19 +19,16 @@ public class MechanumTele extends LinearOpMode {
 //        DcMotor shoulder = hardwareMap.dcMotor.get("shoulder");
 //        DcMotor elbow = hardwareMap.dcMotor.get("elbow");
 
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
 
 //        nom.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 //        shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
