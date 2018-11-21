e7b20696bac48305d7d600245cf1106fbbe14780
Alex Studer <astuder@notetoscreen.com>
2018-11-20 23:04:09 -0500

    test code to call out to native module

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/NativeTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import io.github.thatoddmailbox.atnative.BuildConfig;
+import io.github.thatoddmailbox.atnative.NativeCore;
+
+@Autonomous(name="Native test", group="Tests")
+public class NativeTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        telemetry.addData("getOKString", NativeCore.getOKString());
+        telemetry.addData("Native code version", BuildConfig.VERSION_NAME);
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            idle();
+        }
+    }
+}
