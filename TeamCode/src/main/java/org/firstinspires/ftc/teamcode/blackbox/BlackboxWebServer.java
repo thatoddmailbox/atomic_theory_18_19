9d69c7f016f92ec304b7dffb49506c331f4e10f9
Alex Studer <astuder@notetoscreen.com>
2019-02-26 09:41:39 -0500

    fix bug where graph data couldn't be loaded over phone wifi, and add api to list all matches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blackbox/BlackboxWebServer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blackbox/BlackboxWebServer.java
@@ -3,10 +3,15 @@ package org.firstinspires.ftc.teamcode.blackbox;
 import android.annotation.SuppressLint;
 
 import org.firstinspires.ftc.robotcore.internal.webserver.MimeTypesUtil;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 import fi.iki.elonen.NanoHTTPD;
@@ -18,6 +23,19 @@ public class BlackboxWebServer extends NanoHTTPD {
     public BlackboxWebServer() throws IOException {
         super(4174);
         start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+    }
+
+    public String findMimeType(String path) {
+        String type = MimeTypesUtil.determineMimeType(path);
+        if (type != null) {
+            return type;
+        }
+
+        if (path.endsWith(".csv")) {
+            return "text/csv";
+        }
+
+        return "application/octet-stream";
     }
 
     public Response serveFile(String filePath, String localBasePath) {
@@ -34,17 +52,60 @@ public class BlackboxWebServer extends NanoHTTPD {
 
         try {
             FileInputStream inputStream = new FileInputStream(requestedFile);
-            return newChunkedResponse(Response.Status.OK, MimeTypesUtil.determineMimeType(filePath), inputStream);
+            return newChunkedResponse(Response.Status.OK, findMimeType(filePath), inputStream);
         } catch (FileNotFoundException e) {
             return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/html", "File not found.");
         }
+    }
+
+    public Response listAllLogSessions() {
+        JSONArray logSessionsJSON = new JSONArray();
+
+        File sessionsFile = new File(LogSession.BASE_PATH);
+        String[] folderContents = sessionsFile.list();
+
+        for (String entry : folderContents) {
+            File folderFile = new File(sessionsFile, entry);
+            if (folderFile.isFile()) {
+                continue;
+            }
+
+            File sessionFile = new File(folderFile, "session.json");
+            try {
+                BufferedReader sessionFileReader = new BufferedReader(new FileReader(sessionFile));
+                String sessionFileContents = sessionFileReader.readLine();
+                if (sessionFileContents == null || sessionFileContents.isEmpty()) {
+                    continue;
+                }
+                JSONObject sessionFileJSON = new JSONObject(sessionFileContents);
+                JSONObject logSessionJSON = new JSONObject();
+
+                logSessionJSON.put("path", entry);
+                logSessionJSON.put("phase", sessionFileJSON.get("phase"));
+                logSessionJSON.put("opmode", sessionFileJSON.getJSONArray("contexts").getJSONObject(0).getJSONObject("facts").getString("Name"));
+                logSessionJSON.put("matchType", sessionFileJSON.get("type"));
+                logSessionJSON.put("matchLabel", sessionFileJSON.get("label"));
+                logSessionJSON.put("matchStart", sessionFileJSON.get("matchStart"));
+
+                logSessionsJSON.put(logSessionJSON);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return newFixedLengthResponse(Response.Status.OK, "application/json", logSessionsJSON.toString());
     }
 
     @Override
     public Response serve(IHTTPSession session) {
         String uri = session.getUri();
 
-        if (uri.startsWith("/sessions")) {
+        if (uri.startsWith("/api")) {
+            if (uri.equals("/api/listSessions")) {
+                return listAllLogSessions();
+            }
+            return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/html", "File not found.");
+        } else if (uri.startsWith("/sessions")) {
             String filePath = uri.substring("/sessions".length());
             return serveFile(filePath, LogSession.BASE_PATH);
         } else {
